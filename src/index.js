8ab10f668c13c98728782153c0e650d78df91324
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-03-21 21:53:29 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let ipt = document.querySelector("input");
+let btn = document.querySelector("button");
+let elem = document.querySelector("p");
+btn.addEventListener("click", () => {
+  elem.innerText = ipt.value;
+});
